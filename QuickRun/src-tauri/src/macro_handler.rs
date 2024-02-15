58ee4f8fd86b5f802ea2f2476168e593a1bb4633
Jakub Szamuk <lavaproofJakub1@gmail.com>
2024-02-15 11:07:31 +0000

    Most of rust scaffolding for tauri

---
+++ QuickRun/src-tauri/src/macro_handler.rs
@@ -0,0 +1,232 @@
+
+
+use std::sync::{Arc, Mutex};
+use std::thread;
+use std::time::{Duration, Instant};
+use device_query::{DeviceEvents, DeviceQuery, DeviceState, keymap::Keycode, MousePosition, MouseButton};
+use enigo::{Enigo, Key, KeyboardControllable, MouseControllable};
+
+fn convert_key(key: Keycode) -> Key {
+    match key {
+        Keycode::A => Key::Layout('a'),
+        Keycode::B => Key::Layout('b'),
+        Keycode::C => Key::Layout('c'),
+        Keycode::D => Key::Layout('d'),
+        Keycode::E => Key::Layout('e'),
+        Keycode::F => Key::Layout('f'),
+        Keycode::G => Key::Layout('g'),
+        Keycode::H => Key::Layout('h'),
+        Keycode::I => Key::Layout('i'),
+        Keycode::J => Key::Layout('j'),
+        Keycode::K => Key::Layout('k'),
+        Keycode::L => Key::Layout('l'),
+        Keycode::M => Key::Layout('m'),
+        Keycode::N => Key::Layout('n'),
+        Keycode::O => Key::Layout('o'),
+        Keycode::P => Key::Layout('p'),
+        Keycode::Q => Key::Layout('q'),
+        Keycode::R => Key::Layout('r'),
+        Keycode::S => Key::Layout('s'),
+        Keycode::T => Key::Layout('t'),
+        Keycode::U => Key::Layout('u'),
+        Keycode::V => Key::Layout('v'),
+        Keycode::W => Key::Layout('w'),
+        Keycode::X => Key::Layout('x'),
+        Keycode::Y => Key::Layout('y'),
+        Keycode::Z => Key::Layout('z'),
+        Keycode::Space => Key::Layout(' '),
+        Keycode::Backspace => Key::Backspace,
+        Keycode::Tab => Key::Tab,
+        Keycode::Enter => Key::Return,
+        Keycode::Escape => Key::Escape,
+        Keycode::LControl => Key::Control,
+        Keycode::LShift => Key::Shift,
+        Keycode::LAlt => Key::Alt,
+
+        _ => unimplemented!("Keycode not implemented: {}", key),
+    }
+}
+
+fn convert_mouse_button(button: MouseButton) -> enigo::MouseButton {
+    match button {
+        1 => enigo::MouseButton::Left,
+        2 => enigo::MouseButton::Middle,
+        3 => enigo::MouseButton::Right,
+        _ => unimplemented!("Mouse button not implemented"),
+    }
+}
+
+
+
+
+#[derive(Debug, Clone)]
+enum InputEvent {
+    MouseMove,
+    MouseDown,
+    MouseUp,
+
+    KeyDown,
+    KeyUp,
+}
+
+#[derive(Debug, Clone)]
+struct Step {
+    pub event: EventStep,
+    pub at_time: u128
+}
+#[derive(Debug, Clone)]
+struct EventStep {
+    pub event_type: InputEvent,
+    pub event_data: EventData
+}
+#[derive(Debug, Clone)]
+enum EventData {
+    MouseMove(MousePosition),
+    MouseData(MouseButton),
+    KeyData(Keycode),
+}
+
+
+
+pub struct MacroHandler {
+  stored_actions: Vec<Step>,
+  pub is_recording: bool,
+  pub is_playing: bool
+}
+
+impl MacroHandler {
+  pub fn new() -> MacroHandler {
+    MacroHandler {
+      stored_actions: Vec::new(),
+      is_recording: false,
+      is_playing: true,
+    }
+  }
+
+
+  pub fn start_recording(&mut self) -> bool {
+    self.is_recording = true;
+    let device_state = DeviceState::new();
+
+
+    let actions: Arc<Mutex<Vec<Step>>> = Arc::new(Mutex::new(Vec::new()));
+
+
+    let initial_function_time = Instant::now();
+
+    let _guard = device_state.on_mouse_move({
+        let actions = actions.clone();
+        move |pos: &MousePosition| {
+            actions.lock().unwrap().push(get_current_step(InputEvent::MouseMove, EventData::MouseMove(*pos), &initial_function_time));
+        }
+    });
+    let _guard = device_state.on_mouse_down({
+        let actions = actions.clone();
+        move |mouse_button: &MouseButton| {
+            actions.lock().unwrap().push(get_current_step(InputEvent::MouseDown, EventData::MouseData(*mouse_button), &initial_function_time));
+        }
+    });
+    let _guard = device_state.on_mouse_up({
+        let actions = actions.clone();
+        move |mouse_button: &MouseButton| {
+            actions.lock().unwrap().push(get_current_step(InputEvent::MouseUp, EventData::MouseData(*mouse_button), &initial_function_time));
+        }
+    });
+    let _guard = device_state.on_key_down({
+        let actions = actions.clone();
+        move |key_pressed| {
+            if *key_pressed == Keycode::F12 || *key_pressed == Keycode::F10 {
+                return;
+            }
+            actions.lock().unwrap().push(get_current_step(InputEvent::KeyDown, EventData::KeyData(*key_pressed), &initial_function_time));
+        }
+    });
+    let _guard = device_state.on_key_up({
+        let actions = actions.clone();
+        move |key_pressed| {
+            if *key_pressed == Keycode::F12 || *key_pressed == Keycode::F10 {
+                return;
+            }
+            actions.lock().unwrap().push(get_current_step(InputEvent::KeyUp, EventData::KeyData(*key_pressed), &initial_function_time));
+        }
+    });
+
+
+    loop {
+        if device_state.get_keys().contains(&Keycode::F12) || self.is_recording {
+            break;
+        }
+    };
+
+    let final_actions = actions.lock().unwrap().clone().to_vec();
+    self.stored_actions = final_actions;
+    return true
+  }
+  pub fn stop_recording(&mut self) {
+    self.is_recording = false;
+  }
+
+
+  fn play_macro(&self, speed: &u8) {
+      let mut enigo = Enigo::new();
+      thread::sleep(Duration::from_millis(2000));
+
+      let start_time = Instant::now();
+      for action in self.stored_actions.clone() {
+          wait_for_event(&start_time, action.at_time, &speed);
+          handle_event(&mut enigo, &action.event);
+      }
+  }
+}
+
+
+fn handle_event(enigo: &mut Enigo, event: &EventStep) {
+    match event.event_data {
+        EventData::MouseMove(mouse_pos) => {
+            enigo.mouse_move_to(mouse_pos.0, mouse_pos.1);
+        }
+        EventData::MouseData(mouse_button) => {
+            match event.event_type {
+                InputEvent::MouseDown => {
+                    enigo.mouse_down(convert_mouse_button(mouse_button));
+                }
+                InputEvent::MouseUp => {
+                    enigo.mouse_up(convert_mouse_button(mouse_button));
+                }
+                _ => {}
+            }
+        }
+        EventData::KeyData(keycode) => {
+            match event.event_type {
+                InputEvent::KeyDown => {
+                    enigo.key_down(convert_key(keycode));
+                },
+                InputEvent::KeyUp => {
+                    enigo.key_up(convert_key(keycode));
+                },
+                _ => {}
+            }
+        }
+        _ => {
+            panic!("Unhandled event type");
+        }
+    }
+}
+
+fn get_current_step(event_type: InputEvent, data: EventData, time: &Instant) -> Step {
+    Step {
+        event: EventStep {
+            event_type,
+            event_data: data,
+        },
+        at_time: time.elapsed().as_millis(),
+    }
+}
+
+fn wait_for_event(current_time: &Instant, required_time: u128, speed: &u8) {
+    let mut time = current_time.elapsed().as_millis();
+    while time < required_time / *speed as u128 {
+        time = current_time.elapsed().as_millis();
+        thread::sleep(Duration::from_millis(1));
+    }
+}
--- QuickRun/src-tauri/src/main.rs
+++ QuickRun/src-tauri/src/main.rs
@@ -1,15 +1,28 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+mod macro_handler;
+use macro_handler::MacroHandler;
+
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn start_recording(app_handler: tauri::State<MacroHandler>) {
+    
+}
+#[tauri::command]
+fn stop_recording(app_handler: tauri::State<MacroHandler>) {
+    
+}
+
+#[tauri::command]
+fn play_macro(app_handler: tauri::State<MacroHandler>, speed: u8) {
+    
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .manage(MacroHandler::new())
+        .invoke_handler(tauri::generate_handler![start_recording])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
